9f741e9b34037eae35a22f96b121436b3d42c23c
Christopher MCAdams <cmcadams@groupon.com>
2017-10-22 17:56:13 -0500

    found the pattern to use

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ class Wyndex:
             self
             Optional: list of files
         """
-        previous_commit = self.get_previous_commit()
-        self.__files = self.get_files_with_changes(previous_commit)
+        self.__files = self.get_files_with_changes('HEAD^')
 
     def lint_changes(self):
         """ lint_changes:
@@ -18,14 +17,12 @@ class Wyndex:
         """
         self.__lint_coffee()
         self.__lint_stylus()
-
-        # self.__find_dot_only()
-        # self.__find_debuggers()
+        self.__find_dot_only()
+        #self.__find_debuggers()
 
     def run_tests(self):
         """ docString. """
         __test_files = 'test files!!!!!'
-        print __test_files
         # self.__run_client_tests()
         # self.__run_server_tests()
         # self.__run_integration_tests()
@@ -35,50 +32,59 @@ class Wyndex:
         self.lint_changes()
         # self.run_tests()
 
-
-
     def __lint_coffee(self):
-        # lint coffee
         print '> \33[36m' + 'Linting CoffeeScript...' + '\33[0m'
-        coffee = re.compile('/*.coffee*')
-        coffee_files = filter(coffee.match, self.__files)
-        print 'here', coffee_files
-        # subprocess.call(['./node_modules/.bin/coffeelint', '-f ./coffeelint.json,', coffee_files])
+
+        for coffee_file in self.__files['coffee']:
+            subprocess.call([
+                'coffeelint',
+                '-f', './coffeelint.json',
+                coffee_file], cwd='./')
 
     def __lint_stylus(self):
-        # lint coffee
         print '> \33[36m' + 'Linting Stylus...' + '\33[0m'
-        return self.__files
+
+        for styl_file in self.__files['stylus']:
+            subprocess.call([
+                'coffeelint',
+                '-f', './coffeelint.json',
+                styl_file], cwd='./')
 
     def __find_dot_only(self):
         print '> \33[36m' + 'Checking for `.only`...' + '\33[0m'
-        return self.__files
-        '''
+        test_files = self.__files['test']
+        for test in test_files:
+            with open(test) as test_file:
+                print test_file
+                # TODO
+                """
+                 read line by line for .only
+                  if line =~ /(describe|it|context)\.only/i
+                    line_num += 1
+                    abort "\n Found `.only` \n \33[31m ##{line_num}: \33[0m#{file}"
+                """
 
-        @files.each do |file|
-          if file =~ /test\./i
-            test_file = File.read("#{file}")
-
+        """
+            ...
             test_file.each_line.with_index do |line, line_num|
               if line =~ /(describe|it|context)\.only/i
                 line_num += 1
-                abort "\n Found `.only` \n \e[31m ##{line_num}: \e[0m#{file}"
+                abort "\n Found `.only` \n \33[31m ##{line_num}: \33[0m#{file}"
               end
             end
 
           end
         end
         puts "OK!"
-        '''
+        """
 
     def __find_debuggers(self):
-        print '> \33[36m' +  'Searching for debuggers...' + '\33[0m'
+        print '> \33[36m' + 'Searching for debuggers...' + '\33[0m'
         return self.__files
 
     def __get_test_files(self):
-        print 'files:', self.__files
         regex = re.compile('/*.test.*')
-        x =  filter(regex.match, self.__files)
+        x = filter(regex.match, self.__files)
 
     @staticmethod
     def get_previous_commit():
@@ -96,8 +102,20 @@ class Wyndex:
         get_files = subprocess.check_output([
             'git', 'diff', '--name-only',
             '--diff-filter=ACMRTUXB', previous_commit])
+        files = get_files.split()
 
-        return get_files.split()
+        files_dict = dict()
+        coffee_files = [f for f in files if '.coffee' in f]
+        stylus_files = [f for f in files if '.styl' in f]
+        test_files = [f for f in files if '.test.*' in f]
+
+        files_dict['all'] = files
+        files_dict['coffee'] = coffee_files
+        files_dict['stylus'] = stylus_files
+        files_dict['test'] = test_files
+
+        return files_dict
+
 
     @staticmethod
     def __run_client_tests():
