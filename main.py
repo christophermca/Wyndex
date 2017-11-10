7e106ff529a6e459caa60c1150f2959a78b26b6c
Christopher MCAdams <cmcadams@groupon.com>
2017-11-09 18:11:27 -0600

    Structure changes, creating CLI

--- main.py
+++ main.py
@@ -1,83 +1,111 @@
 import subprocess
-import sys
 import re
 
 class Wyndex:
-    def __init__(self):
+    # TODO
+    """
+    Pass flag to change if module will respect the returned exitcode
+    """
+    def __init__(self, *args, **kwargs):
         """
         Arguments:
-            self
-            Optional: list of files
+            *args:
+                file(s)    # One or multiple files
+
+            **kwargs:
+               --exitcode    # Use exitcode
+               --tdd         # Run tests for script changes
+               --skip-tests  # Skip tests
         """
-        self.__files = self.get_files_with_changes('HEAD^')
+
+        if kwargs['SHA']:
+            files = self.get_files_with_changes(kwargs['SHA'])
+            self.__files = self.__sort_files(files)
+        else:
+            self.__files = self.__sort_files(args)
 
 
     @classmethod
     def lint_changes(cls):
         """ lint_changes:
-        lint files --  stylus,  coffee
-        checks for --  dot_only, debuggers
+        lint files -- stylus, coffee, javascript
+        checks for -- dot_only, debuggers
         """
         cls().__lint_coffee()
+        cls().__lint_js()
         cls().__lint_stylus()
         cls().__find_dot_only()
         cls().__find_debuggers()
 
-    def run_tests(self):
+    @classmethod
+    def run_tests(cls):
         """ Runs Client, Server, Integration tests. """
-        self.__run_server_tests()
-        self.__run_client_tests()
-        # self.__run_integration_tests()
-
+        cls().__run_server_tests()
+        cls().__run_client_tests()
+        # cls().__run_integration_tests()
 
     @classmethod
     def clean_code(cls):
         """ Runs Linter and Checks tests """
         cls().lint_changes()
         cls().run_tests()
-        print '\n>> \33[32m' + 'Complete.\n' + '\33[0m'
+        print('\n>> \33[32m' + 'Complete.\n' + '\33[0m')
 
     @staticmethod
-    def get_previous_commit():
-        if subprocess.call(['git', 'rev-parse', '--verify HEAD']):
-            against = 'HEAD'
-        else:
-            against = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
-
-        return against
-
-    @staticmethod
-    def get_files_with_changes(previous_commit):
+    def get_files_with_changes(SHA):
         """ @Returns: A list of files with changes """
 
         get_files = subprocess.check_output([
             'git', 'diff', '--name-only',
-            '--diff-filter=ACMRTUXB', previous_commit])
-        files = get_files.split()
+            '--diff-filter=ACMRTUXB', SHA])
 
+        return get_files.split()
+
+    @staticmethod
+    def __sort_files(files):
+        """ Setup configuration file
+            contains:
+            - filetypes
+            - linting command
+        """
         files_dict = dict()
         coffee_files = [f for f in files if '.coffee' in f]
-        stylus_files = [f for f in files if '.styl' in f]
-        test_files = [f for f in files if '.test.' in f]
+        js_files = [f for f in files if bool(re.search('(?<!.test).js$', f))]
+        stylus_files = [f for f in files if bool(re.search(r'.styl$', f))]
+        test_files = [f for f in files
+                      if bool(re.search(r'.test.[coffee|js]', f))]
 
         files_dict['all'] = files
-        files_dict['coffee'] = coffee_files
+        files_dict['scripts'] = {
+            'coffee': coffee_files,
+            'js': js_files
+        }
         files_dict['stylus'] = stylus_files
         files_dict['test'] = test_files
 
         return files_dict
 
-    def __lint_coffee(self):
-        print '> \33[36m' + 'Linting CoffeeScript...' + '\33[0m'
+    def __lint_js(self):
+        print('> \33[36m' + 'Linting Javascript...' + '\33[0m')
 
-        for coffee_file in self.__files['coffee']:
+        for js_file in self.__files['scripts']['js']:
+            subprocess.call([
+                'eslint',
+                '--ignore-path',
+                '.gitignore',
+                js_file], cwd='./')
+
+    def __lint_coffee(self):
+        print('> \33[36m' + 'Linting CoffeeScript...' + '\33[0m')
+
+        for coffee_file in self.__files['scripts']['coffee']:
             subprocess.call([
                 'coffeelint',
                 '-f', './coffeelint.json',
                 coffee_file], cwd='./')
 
     def __lint_stylus(self):
-        print '> \33[36m' + 'Linting Stylus...' + '\33[0m'
+        print('> \33[36m' + 'Linting Stylus...' + '\33[0m')
 
         for styl_file in self.__files['stylus']:
             subprocess.call([
@@ -86,32 +114,36 @@ class Wyndex:
                 styl_file], cwd='./')
 
     def __find_dot_only(self):
-        print '> \33[36m' + 'Searching for `.only`...' + '\33[0m'
+        print('> \33[36m' + 'Searching for `.only`...' + '\33[0m')
 
         for test in self.__files['test']:
-            with open(test) as test_file:
-                for idx, line in enumerate(test_file):
-                    if re.match("(it|describe|context)\.only", line) is not None:
-                        print('Found `.only`' + '\33[33m #{}:\33[31m {}\33[0m \n'.format(idx, test))
+            with open(test) as f:
+                for idx, line in enumerate(f):
+                    if re.match("\s*(it|describe|context)\.only", line) is not None:
+                        print('Found `.only`')
+                        print('\33[33m #{}:\33[31m {}\33[0m \n'
+                              .format((idx + 1), test))
 
     def __find_debuggers(self):
-        print '> \33[36m' + 'Searching for debuggers...' + '\33[0m'
+        print('> \33[36m' + 'Searching for debuggers...' + '\33[0m')
+        print self.__files
 
-        regex = re.compile('(\.coffee|\.js)$')
-        script_files = filter(regex.search, self.__files['all'])
-
-        for script in script_files:
-            with open(script):
-                for idx, line in enumerate(script):
-                    if re.match("debugger", line) is not None:
-                        print('Found `debugger`' + '\33[33m #{}:\33[31m {}\33[0m \n'.format(idx, file))
+        for scriptType in self.__files['scripts']:
+            scripts = self.__files['scripts'][scriptType]
+            for script in scripts:
+                with open(script) as f:
+                    for idx, line in enumerate(f):
+                        if re.search("debugger", line) is not None:
+                            print('\33[31mFound Debuggers:\33[0m')
+                            print('\33[33m #{}:\33[31m {}\33[0m \n'
+                                  .format((idx + 1), f.name))
 
     def __run_integration_tests(self):
         regex = re.compile('/integration/')
         integration_tests = filter(regex.search, self.__files['test'])
 
         if len(integration_tests) > 0:
-            print '> \33[36m' + 'Running Integration Tests...' + '\33[0m'
+            print('> \33[36m' + 'Running Integration Tests...' + '\33[0m')
 
             for test in integration_tests:
                 subprocess.check_output(['mocha', test])
@@ -121,7 +153,7 @@ class Wyndex:
         server_tests = filter(regex.search, self.__files['test'])
 
         if len(server_tests) > 0:
-            print '> \33[36m' + 'Running Server Tests...' + '\33[0m'
+            print('> \33[36m' + 'Running Server Tests...' + '\33[0m')
 
             for test in server_tests:
                 subprocess.check_output(['mocha', test])
@@ -133,6 +165,3 @@ class Wyndex:
         if len(client_files) > 0:
             print '> \33[36m' + 'Running Client Tests...' + '\33[0m'
             subprocess.call(['make', 'test-client'])
-
-
-Wyndex.lint_changes()
---
+++ wyndex.py
@@ -0,0 +1,23 @@
+from wyndex import main as wyndex
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='Wyndex',
+    description='A reporter, helping keep your code clean')
+
+parser.add_argument('files', metavar='file', nargs='*',
+                    help='file(s) to run Wyndex against')
+
+parser.add_argument('--skip-tests', action='store_true', help='skip tests')
+
+parser.add_argument('-c', '--commit', metavar='SHA', nargs='?',
+                    dest='SHA', const='HEAD^', required=False,
+                    help='use SHA to get files')
+
+args = parser.parse_args()
+
+if args.SHA:
+    wyndex(SHA=args.SHA)
+else:
+    wyndex(args.files)
+
