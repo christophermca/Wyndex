b63668de177eab2c9e4c044f5986380b3bc1e99d
Christopher MCAdams <mca.christopher@gmail.com>
2021-04-12 23:08:46 -0400

    clean up cli files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wyndex", # Replace with your own username
+    name="wyndex",
     version="0.0.1",
     author="Christopher McAdams",
     author_email="mca.christopher@gmail.com",
